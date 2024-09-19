2dfc112a623dfdd7e22452204803b25d4d67fb17
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2024-09-19 13:35:59 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const inputMail = document.querySelector('input[type="email"]');
+const inputPassword = document.querySelector('input[type="password"]');
+const btnLogin = document.querySelector('.btn-login');
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputMail.value === 'abc@gmail.com' &&
+    Number(inputPassword.value) === 1234
+  ) {
+    window.location.href = '/home.html';
+  } else {
+    alert('Your email or password is incorrect');
+  }
+});
